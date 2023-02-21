c7b7d289315c3e830f4b6c086fee4df7b56eb68e
Seunelvis1 <adeniranoluwaseunelvis@gmail.com>
2023-02-20 20:39:01 -0800

    worked

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*/
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	putchar('\n');
+	return (0);
+}
