5460fd3c4c9c4fefb02632ef9824da65df33d662
Seunelvis1 <adeniranoluwaseunelvis@gmail.com>
2023-02-20 19:57:02 -0800

    testing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - return alphabets
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int ch = 'a';
+	while (ch <= 'z')
+	{
+	printf(ch);
+	ch++;
+	}
+	putchar('\n');
+	return (0);
+}
