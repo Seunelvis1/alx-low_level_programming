3d13ba74f2eee456fcb06fdbd3f5202797ceb857
Seunelvis1 <adeniranoluwaseunelvis@gmail.com>
2023-03-15 21:12:07 -0700

    0x09. C - Static libraries

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -9,34 +9,3 @@ int _isdigit(int c)
 {
 	return (c >= '0' && c <= '9');
 }
- 
- 
- ============================
- 
- 
-1-memcpy.c
-
-
-#include "main.h"
-/**
- *_memcpy - a function that copies memory area
- *@dest: memory where is stored
- *@src: memory where is copied
- *@n: number of bytes
- *
- *Return: copied memory with n byted changed
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	int r = 0;
-	int i = n;
-
-
-	for (; r < i; r++)
-	{
-		dest[r] = src[r];
-		n--;
-	}
-	return (dest);
-}
-
--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -17,33 +17,3 @@ char *_strchr(char *s, char c)
 	}
 	return (0);
 }
-
-
-
-
- 
-============================
- 
-2-strlen.c
-
-
-#include "main.h"
-/**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
- */
-int _strlen(char *s)
-{
-	int longi = 0;
-
-
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
-
-
-	return (longi);
-}
